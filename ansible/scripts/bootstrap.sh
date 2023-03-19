c0912f7f2b8f1427941b3d95be03a6bd0a7178a4
Truong Cao Xuan <truongxuanc@gmail.com>
2023-03-19 21:50:03 +0700

    vagrant box collection

---
+++ ansible/scripts/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# vagrant by default creates its own keypair for all the machines. Password based authentication will be disabled by default and enabling it so password based auth can be done.
+
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# Supressing the banner message everytime you connect to the vagrant box.
+
+touch /home/vagrant/.hushlogin
+
+# Updating the hosts file for all the 4 nodes with the IP given in vagrantfile
+
+# 192.168.56.10 manager.ansible.com manager
+# 192.168.56.11 controller.ansible.com controller
+# 192.168.56.12 worker1.ansible.com worker1
+# 192.168.56.13 worker2.ansible.com worker2
+
+echo -e "192.168.56.10 manager.anslab.com manager\n192.168.56.11 controller.anslab.com controller\n192.168.56.12 worker1.anslab.com worker1\n192.168.56.13 worker2.anslab.com worker2" >> /etc/hosts
+
+# Installing necessary packages 
+
+sudo apt-get update
+sudo apt-get -y install curl wget net-tools iputils-ping python3-pip sshpass
+
+# Install ansible using pip only in manager node
+
+if [[ $(hostname) = "manager" ]]; then
+  sudo pip3 install ansible
+fi
---
+++ terraform-ansible/README.md
@@ -0,0 +1,10 @@
+# DevOps box
+* A vagrant project with an ubuntu box with the tools needed to do DevOps
+
+# tools included
+* Terraform
+* AWS CLI
+* Ansible
+
+fork from 
+- https://github.com/wardviaene/devops-box
